18dfab7a613e29820581bbc4f5871cfec778fbbf
Tejas-1403 <tejasjogale22@gmail.com>
2024-04-28 18:57:53 +0530

    API-Gateway logger fixed

--- api-gateway/src/main/java/com/microservices/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/microservices/apigateway/ApiGatewayApplication.java
@@ -3,10 +3,13 @@ package com.microservices.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.core.publisher.Hooks;
+
 @SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
+	    Hooks.enableAutomaticContextPropagation();
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 	
